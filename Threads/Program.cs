b072269d419eb17053160d4a9be80726e1ebfb66
Kanevry <bernhardgoetzendorfer95@gmail.com>
2017-01-26 11:27:26 +0100

    initial commit

---
+++ Threads/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Threads
+{
+    public static class Program
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: " + i);
+                Thread.Sleep(700); // Sleep, Die Zeit wie lange das Programm warten soll, 
+                                   // bis das nächste Statement ausgeführt wird
+            }
+        }
+
+
+
+
+        public static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod)); //Erzeuge einen Thread den ich kontrollieren kann. (Status, priorität...)
+            t.Start();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread.");
+                Thread.Sleep(700);
+            }
+
+            //t.Join(); //Was macht das? - Laut Buch: The Thread.Join method is called on the main thread to let it wait until the other thread finishes.
+
+
+
+        }
+    }
+}
---
+++ Threads2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Threads2
+{
+    class Program
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: " + i);
+                Thread.Sleep(700); // Sleep, Die Zeit wie lange das Programm warten soll, 
+                                   // bis das nächste Statement ausgeführt wird
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart())
+
+
+
+        }
+    }
+}
